95cd86f1d16c4c19f298b78556ccf6944d6e1231
Hughes <hughespham2002the2nd@yahoo.com>
2021-12-03 11:38:08 -0600

    done 2

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const PORT = process.env.PORT || 9000
 server.use(express.json())
 server.use(cors())
 
-server.get('/api/bobby', (req, res)=>{
+server.get('/api/Hughes', (req, res)=>{
   res.json({
-      message:"Bobbys Api Is Working"
+      message:"Hughes's Api Is Working"
   })
 })
 
 server.use('*', (req, res)=>{
-    res.send(`<h1> Welcome to Bobby's API! </h1>`)
+    res.send(`<h1> Welcome to Hughes's API! </h1>`)
 })
 
 
